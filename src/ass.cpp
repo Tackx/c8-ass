ded1f7c309c3188da99a6f3c558fe445c96b4285
Tackx <tack@vxn.dev>
2026-09-11 00:39:40 +0200

    Add output path flag support

--- src/ass.cpp
+++ src/ass.cpp
@@ -8,6 +8,7 @@
 #include <sstream>
 #include <stdexcept>
 #include <string>
+#include <string_view>
 #include <utility>
 
 #ifdef _WIN32
@@ -30,46 +31,99 @@ namespace ass
 int assemble(int argc, char** args)
 {
     static constexpr auto USAGE_MSG = "Usage: ass INPUT OUTPUT";
-    static constexpr auto DEFAULT_OUT_PATH = "./output.ch8";
 
-    // TODO: Enable specifying output path as a flag (-o)
+    std::string inPath{};
+    std::string outPath{"./output.ch8"};
+
+    // Arg count can be:
+    // - 1 (executable name) -- ass
+    // - 2 (exec. name + input path) -- ass input.ass
+    // - 3 (exec. name + optional output flag) -- ass -o foo.ch8 < input.ass
+    // - 4 (exec. name + optional output flag + input path) -- ass -o foo.ch8 input.ass
     try
     {
         std::stringstream buffer;
         std::string fileContent;
 
-        if (argc <= 1 && isatty(fileno(stdin)))
-        {
-            std::println("{}", USAGE_MSG);
+        bool isFileInput = !isatty(fileno(stdin));
 
-            throw std::runtime_error("No input file path provided.");
-        }
-
-        else if (argc >= 2)
+        switch (argc)
         {
+        case 2:
+        {
+            // TODO: Validate the arg is not a flag (since it does not make sense to only have a dangling flag)
+
             // Try to load input from args[1]
-            {
-                std::ifstream t(args[1], std::ios::binary);
-                if (!t)
-                {
-                    throw std::runtime_error("Failed to open file.");
-                }
+            inPath = args[1];
 
-                buffer << t.rdbuf();
-            }
+            break;
         }
 
-        else if (argc >= 1)
+        case 3:
+        {
+            // TODO: We need to make sure that the second arg is a (supported) flag and the third arg is a filepath and that we have a file input stream
+            std::string_view flag{args[1]};
+            std::string_view arg{args[2]};
+
+            if (flag != "-o")
+            {
+                throw std::runtime_error(std::format("Unsupported flag {}", flag));
+            }
+
+            outPath = arg;
+
+            break;
+        }
+
+        case 4:
+        {
+            std::string_view flag{args[1]};
+            std::string_view arg1{args[2]};
+            std::string_view arg2{args[3]};
+
+            if (flag != "-o")
+            {
+                throw std::runtime_error(std::format("Unsupported flag {}", flag));
+            }
+
+            outPath = arg1;
+            inPath = arg2;
+
+            break;
+        }
+
+        default:
+            // Throw too many flags?
+            break;
+        }
+
+        if (!inPath.empty())
+        {
+            std::ifstream t(inPath, std::ios::binary);
+            if (!t)
+            {
+                throw std::runtime_error("Failed to open file.");
+            }
+
+            buffer << t.rdbuf();
+        }
+        else if (isFileInput)
         {
             // Try to load input from stdin
             buffer << std::cin.rdbuf();
+        }
+        else
+        {
+            std::println("{}", USAGE_MSG);
+
+            throw std::runtime_error("No input specified.");
         }
 
         fileContent = std::move(buffer).str();
 
         Lexer lexer{fileContent};
         Parser parser{fileContent};
-        Emitter emitter{DEFAULT_OUT_PATH};
+        Emitter emitter{outPath};
 
         auto tokens = lexer.getTokens();
 
