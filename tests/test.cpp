efa5496e79a1d655b9547159bb927bb6351ec8d2
Tackx <tack@vxn.dev>
2026-09-21 23:43:22 +0200

    Add instruction parsing tests

--- tests/test.cpp
+++ tests/test.cpp
@@ -1,43 +1,763 @@
-//              Copyright Catch2 Authors
-// Distributed under the Boost Software License, Version 1.0.
-//   (See accompanying file LICENSE.txt or copy at
-//        https://www.boost.org/LICENSE_1_0.txt)
-
-// SPDX-License-Identifier: BSL-1.0
-
-// 010-TestCase.cpp
-// And write tests in the same file:
 #include <catch2/catch_test_macros.hpp>
+#include <variant>
 
-static int Factorial(int number)
+#include "instruction.h"
+#include "lexer.h"
+#include "loader.h"
+#include "parser.h"
+
+TEST_CASE("CLS", "[parser]")
 {
-    return number <= 1 ? number : Factorial(number - 1) * number; // fail
-    // return number <= 1 ? 1      : Factorial( number - 1 ) * number;  // pass
+    ass::filename = "<test>";
+
+    ass::Lexer l{"CLS"};
+    const auto tokens = l.produceTokens();
+
+    ass::Parser p{};
+    p.parseLabels(tokens);
+    auto output = p.parseInstructions(tokens);
+
+    REQUIRE(output.size() == 1);
+
+    auto instr = output[0];
+    REQUIRE(std::holds_alternative<ass::Instruction>(instr));
+
+    auto val = std::get<ass::Instruction>(instr);
+
+    REQUIRE(val.encodedHex == 0x00E0);
 }
 
-TEST_CASE("Factorial of 0 is 1 (fail)", "[single-file]")
+TEST_CASE("CLS with a newline", "[parser]")
 {
-    REQUIRE(Factorial(0) == 1);
+    ass::filename = "<test>";
+
+    ass::Lexer l{"CLS\n"};
+    const auto tokens = l.produceTokens();
+
+    ass::Parser p{};
+    p.parseLabels(tokens);
+    auto output = p.parseInstructions(tokens);
+
+    REQUIRE(output.size() == 1);
+
+    auto instr = output[0];
+    REQUIRE(std::holds_alternative<ass::Instruction>(instr));
+
+    auto val = std::get<ass::Instruction>(instr);
+
+    REQUIRE(val.encodedHex == 0x00E0);
 }
 
-TEST_CASE("Factorials of 1 and higher are computed (pass)", "[single-file]")
+TEST_CASE("ADD 7xnn", "[parser]")
 {
-    REQUIRE(Factorial(1) == 1);
-    REQUIRE(Factorial(2) == 2);
-    REQUIRE(Factorial(3) == 6);
-    REQUIRE(Factorial(10) == 3628800);
+    ass::filename = "<test>";
+
+    ass::Lexer l{"ADD V0, 123"};
+    const auto tokens = l.produceTokens();
+
+    ass::Parser p{};
+    p.parseLabels(tokens);
+    auto output = p.parseInstructions(tokens);
+
+    REQUIRE(output.size() == 1);
+
+    auto instr = output[0];
+    REQUIRE(std::holds_alternative<ass::Instruction>(instr));
+
+    auto val = std::get<ass::Instruction>(instr);
+
+    REQUIRE(val.encodedHex == 0x707B);
 }
 
-// Compile & run:
-// - g++ -std=c++14 -Wall -I$(CATCH_SINGLE_INCLUDE) -o 010-TestCase 010-TestCase.cpp && 010-TestCase --success
-// - cl -EHsc -I%CATCH_SINGLE_INCLUDE% 010-TestCase.cpp && 010-TestCase --success
+TEST_CASE("ADD 8xy4", "[parser]")
+{
+    ass::filename = "<test>";
 
-// Expected compact output (all assertions):
-//
-// prompt> 010-TestCase --reporter compact --success
-// 010-TestCase.cpp:14: failed: Factorial(0) == 1 for: 0 == 1
-// 010-TestCase.cpp:18: passed: Factorial(1) == 1 for: 1 == 1
-// 010-TestCase.cpp:19: passed: Factorial(2) == 2 for: 2 == 2
-// 010-TestCase.cpp:20: passed: Factorial(3) == 6 for: 6 == 6
-// 010-TestCase.cpp:21: passed: Factorial(10) == 3628800 for: 3628800 (0x375f00) == 3628800 (0x375f00)
-// Failed 1 test case, failed 1 assertion.
+    ass::Lexer l{"ADD V0, V1"};
+    const auto tokens = l.produceTokens();
+
+    ass::Parser p{};
+    p.parseLabels(tokens);
+    auto output = p.parseInstructions(tokens);
+
+    REQUIRE(output.size() == 1);
+
+    auto instr = output[0];
+    REQUIRE(std::holds_alternative<ass::Instruction>(instr));
+
+    auto val = std::get<ass::Instruction>(instr);
+
+    REQUIRE(val.encodedHex == 0x8014);
+}
+
+TEST_CASE("ADD Fx1E", "[parser]")
+{
+    ass::filename = "<test>";
+
+    ass::Lexer l{"ADD I, V1"};
+    const auto tokens = l.produceTokens();
+
+    ass::Parser p{};
+    p.parseLabels(tokens);
+    auto output = p.parseInstructions(tokens);
+
+    REQUIRE(output.size() == 1);
+
+    auto instr = output[0];
+    REQUIRE(std::holds_alternative<ass::Instruction>(instr));
+
+    auto val = std::get<ass::Instruction>(instr);
+
+    REQUIRE(val.encodedHex == 0xF11E);
+}
+
+TEST_CASE("AND 8xy2", "[parser]")
+{
+    ass::filename = "<test>";
+
+    ass::Lexer l{"AND V1, V4"};
+    const auto tokens = l.produceTokens();
+
+    ass::Parser p{};
+    p.parseLabels(tokens);
+    auto output = p.parseInstructions(tokens);
+
+    REQUIRE(output.size() == 1);
+
+    auto instr = output[0];
+    REQUIRE(std::holds_alternative<ass::Instruction>(instr));
+
+    auto val = std::get<ass::Instruction>(instr);
+
+    REQUIRE(val.encodedHex == 0x8142);
+}
+
+TEST_CASE("CALL 2nnn hex", "[parser]")
+{
+    ass::filename = "<test>";
+
+    ass::Lexer l{"CALL 0x123"};
+    const auto tokens = l.produceTokens();
+
+    ass::Parser p{};
+    p.parseLabels(tokens);
+    auto output = p.parseInstructions(tokens);
+
+    REQUIRE(output.size() == 1);
+
+    auto instr = output[0];
+    REQUIRE(std::holds_alternative<ass::Instruction>(instr));
+
+    auto val = std::get<ass::Instruction>(instr);
+
+    REQUIRE(val.encodedHex == 0x2123);
+}
+
+TEST_CASE("CALL 2nnn decimal", "[parser]")
+{
+    ass::filename = "<test>";
+
+    ass::Lexer l{"CALL 123"};
+    const auto tokens = l.produceTokens();
+
+    ass::Parser p{};
+    p.parseLabels(tokens);
+    auto output = p.parseInstructions(tokens);
+
+    REQUIRE(output.size() == 1);
+
+    auto instr = output[0];
+    REQUIRE(std::holds_alternative<ass::Instruction>(instr));
+
+    auto val = std::get<ass::Instruction>(instr);
+
+    REQUIRE(val.encodedHex == 0x207B);
+}
+
+TEST_CASE("DRW Dxyn", "[parser]")
+{
+    ass::filename = "<test>";
+
+    ass::Lexer l{"DRW V0, V1, 5"};
+    const auto tokens = l.produceTokens();
+
+    ass::Parser p{};
+    p.parseLabels(tokens);
+    auto output = p.parseInstructions(tokens);
+
+    REQUIRE(output.size() == 1);
+
+    auto instr = output[0];
+    REQUIRE(std::holds_alternative<ass::Instruction>(instr));
+
+    auto val = std::get<ass::Instruction>(instr);
+
+    REQUIRE(val.encodedHex == 0xD015);
+}
+
+TEST_CASE("JP 1nnn", "[parser]")
+{
+    ass::filename = "<test>";
+
+    ass::Lexer l{"JP 0x123"};
+    const auto tokens = l.produceTokens();
+
+    ass::Parser p{};
+    p.parseLabels(tokens);
+    auto output = p.parseInstructions(tokens);
+
+    REQUIRE(output.size() == 1);
+
+    auto instr = output[0];
+    REQUIRE(std::holds_alternative<ass::Instruction>(instr));
+
+    auto val = std::get<ass::Instruction>(instr);
+
+    REQUIRE(val.encodedHex == 0x1123);
+}
+
+TEST_CASE("JP0 1nnn", "[parser]")
+{
+    ass::filename = "<test>";
+
+    ass::Lexer l{"JP0 0x123"};
+    const auto tokens = l.produceTokens();
+
+    ass::Parser p{};
+    p.parseLabels(tokens);
+    auto output = p.parseInstructions(tokens);
+
+    REQUIRE(output.size() == 1);
+
+    auto instr = output[0];
+    REQUIRE(std::holds_alternative<ass::Instruction>(instr));
+
+    auto val = std::get<ass::Instruction>(instr);
+
+    REQUIRE(val.encodedHex == 0xB123);
+}
+
+TEST_CASE("LD 6xnn", "[parser]")
+{
+    ass::filename = "<test>";
+
+    ass::Lexer l{"LD V0, 0xFF"};
+    const auto tokens = l.produceTokens();
+
+    ass::Parser p{};
+    p.parseLabels(tokens);
+    auto output = p.parseInstructions(tokens);
+
+    REQUIRE(output.size() == 1);
+
+    auto instr = output[0];
+    REQUIRE(std::holds_alternative<ass::Instruction>(instr));
+
+    auto val = std::get<ass::Instruction>(instr);
+
+    REQUIRE(val.encodedHex == 0x60FF);
+}
+
+TEST_CASE("LD 8xy0", "[parser]")
+{
+    ass::filename = "<test>";
+
+    ass::Lexer l{"LD V0, V1"};
+    const auto tokens = l.produceTokens();
+
+    ass::Parser p{};
+    p.parseLabels(tokens);
+    auto output = p.parseInstructions(tokens);
+
+    REQUIRE(output.size() == 1);
+
+    auto instr = output[0];
+    REQUIRE(std::holds_alternative<ass::Instruction>(instr));
+
+    auto val = std::get<ass::Instruction>(instr);
+
+    REQUIRE(val.encodedHex == 0x8010);
+}
+
+TEST_CASE("LD Annn", "[parser]")
+{
+    ass::filename = "<test>";
+
+    ass::Lexer l{"LD I, 0x123"};
+    const auto tokens = l.produceTokens();
+
+    ass::Parser p{};
+    p.parseLabels(tokens);
+    auto output = p.parseInstructions(tokens);
+
+    REQUIRE(output.size() == 1);
+
+    auto instr = output[0];
+    REQUIRE(std::holds_alternative<ass::Instruction>(instr));
+
+    auto val = std::get<ass::Instruction>(instr);
+
+    REQUIRE(val.encodedHex == 0xA123);
+}
+
+TEST_CASE("LD Fx07", "[parser]")
+{
+    ass::filename = "<test>";
+
+    ass::Lexer l{"LD V0, DT"};
+    const auto tokens = l.produceTokens();
+
+    ass::Parser p{};
+    p.parseLabels(tokens);
+    auto output = p.parseInstructions(tokens);
+
+    REQUIRE(output.size() == 1);
+
+    auto instr = output[0];
+    REQUIRE(std::holds_alternative<ass::Instruction>(instr));
+
+    auto val = std::get<ass::Instruction>(instr);
+
+    REQUIRE(val.encodedHex == 0xF007);
+}
+
+TEST_CASE("LD Fx0A", "[parser]")
+{
+    ass::filename = "<test>";
+
+    ass::Lexer l{"LD V0, K"};
+    const auto tokens = l.produceTokens();
+
+    ass::Parser p{};
+    p.parseLabels(tokens);
+    auto output = p.parseInstructions(tokens);
+
+    REQUIRE(output.size() == 1);
+
+    auto instr = output[0];
+    REQUIRE(std::holds_alternative<ass::Instruction>(instr));
+
+    auto val = std::get<ass::Instruction>(instr);
+
+    REQUIRE(val.encodedHex == 0xF00A);
+}
+
+TEST_CASE("LD Fx15", "[parser]")
+{
+    ass::filename = "<test>";
+
+    ass::Lexer l{"LD DT, V7"};
+    const auto tokens = l.produceTokens();
+
+    ass::Parser p{};
+    p.parseLabels(tokens);
+    auto output = p.parseInstructions(tokens);
+
+    REQUIRE(output.size() == 1);
+
+    auto instr = output[0];
+    REQUIRE(std::holds_alternative<ass::Instruction>(instr));
+
+    auto val = std::get<ass::Instruction>(instr);
+
+    REQUIRE(val.encodedHex == 0xF715);
+}
+
+TEST_CASE("LD Fx18", "[parser]")
+{
+    ass::filename = "<test>";
+
+    ass::Lexer l{"LD ST, VF"};
+    const auto tokens = l.produceTokens();
+
+    ass::Parser p{};
+    p.parseLabels(tokens);
+    auto output = p.parseInstructions(tokens);
+
+    REQUIRE(output.size() == 1);
+
+    auto instr = output[0];
+    REQUIRE(std::holds_alternative<ass::Instruction>(instr));
+
+    auto val = std::get<ass::Instruction>(instr);
+
+    REQUIRE(val.encodedHex == 0xFF18);
+}
+
+TEST_CASE("LD Fx29", "[parser]")
+{
+    ass::filename = "<test>";
+
+    ass::Lexer l{"LD F, VA"};
+    const auto tokens = l.produceTokens();
+
+    ass::Parser p{};
+    p.parseLabels(tokens);
+    auto output = p.parseInstructions(tokens);
+
+    REQUIRE(output.size() == 1);
+
+    auto instr = output[0];
+    REQUIRE(std::holds_alternative<ass::Instruction>(instr));
+
+    auto val = std::get<ass::Instruction>(instr);
+
+    REQUIRE(val.encodedHex == 0xFA29);
+}
+
+TEST_CASE("LD Fx33", "[parser]")
+{
+    ass::filename = "<test>";
+
+    ass::Lexer l{"LD B, V5"};
+    const auto tokens = l.produceTokens();
+
+    ass::Parser p{};
+    p.parseLabels(tokens);
+    auto output = p.parseInstructions(tokens);
+
+    REQUIRE(output.size() == 1);
+
+    auto instr = output[0];
+    REQUIRE(std::holds_alternative<ass::Instruction>(instr));
+
+    auto val = std::get<ass::Instruction>(instr);
+
+    REQUIRE(val.encodedHex == 0xF533);
+}
+
+TEST_CASE("LD Fx55", "[parser]")
+{
+    ass::filename = "<test>";
+
+    ass::Lexer l{"LD [I], V8"};
+    const auto tokens = l.produceTokens();
+
+    ass::Parser p{};
+    p.parseLabels(tokens);
+    auto output = p.parseInstructions(tokens);
+
+    REQUIRE(output.size() == 1);
+
+    auto instr = output[0];
+    REQUIRE(std::holds_alternative<ass::Instruction>(instr));
+
+    auto val = std::get<ass::Instruction>(instr);
+
+    REQUIRE(val.encodedHex == 0xF855);
+}
+
+TEST_CASE("LD Fx65", "[parser]")
+{
+    ass::filename = "<test>";
+
+    ass::Lexer l{"LD v3, [i]"};
+    const auto tokens = l.produceTokens();
+
+    ass::Parser p{};
+    p.parseLabels(tokens);
+    auto output = p.parseInstructions(tokens);
+
+    REQUIRE(output.size() == 1);
+
+    auto instr = output[0];
+    REQUIRE(std::holds_alternative<ass::Instruction>(instr));
+
+    auto val = std::get<ass::Instruction>(instr);
+
+    REQUIRE(val.encodedHex == 0xF365);
+}
+
+TEST_CASE("OR 8xy1", "[parser]")
+{
+    ass::filename = "<test>";
+
+    ass::Lexer l{"OR V1, V9"};
+    const auto tokens = l.produceTokens();
+
+    ass::Parser p{};
+    p.parseLabels(tokens);
+    auto output = p.parseInstructions(tokens);
+
+    REQUIRE(output.size() == 1);
+
+    auto instr = output[0];
+    REQUIRE(std::holds_alternative<ass::Instruction>(instr));
+
+    auto val = std::get<ass::Instruction>(instr);
+
+    REQUIRE(val.encodedHex == 0x8191);
+}
+
+TEST_CASE("RET 00EE", "[parser]")
+{
+    ass::filename = "<test>";
+
+    ass::Lexer l{"RET"};
+    const auto tokens = l.produceTokens();
+
+    ass::Parser p{};
+    p.parseLabels(tokens);
+    auto output = p.parseInstructions(tokens);
+
+    REQUIRE(output.size() == 1);
+
+    auto instr = output[0];
+    REQUIRE(std::holds_alternative<ass::Instruction>(instr));
+
+    auto val = std::get<ass::Instruction>(instr);
+
+    REQUIRE(val.encodedHex == 0x00EE);
+}
+
+TEST_CASE("RND Cxnn", "[parser]")
+{
+    ass::filename = "<test>";
+
+    ass::Lexer l{"RND V2, 0xAF"};
+    const auto tokens = l.produceTokens();
+
+    ass::Parser p{};
+    p.parseLabels(tokens);
+    auto output = p.parseInstructions(tokens);
+
+    REQUIRE(output.size() == 1);
+
+    auto instr = output[0];
+    REQUIRE(std::holds_alternative<ass::Instruction>(instr));
+
+    auto val = std::get<ass::Instruction>(instr);
+
+    REQUIRE(val.encodedHex == 0xC2AF);
+}
+
+TEST_CASE("SE 3xnn", "[parser]")
+{
+    ass::filename = "<test>";
+
+    ass::Lexer l{"SE V0, 0x1E"};
+    const auto tokens = l.produceTokens();
+
+    ass::Parser p{};
+    p.parseLabels(tokens);
+    auto output = p.parseInstructions(tokens);
+
+    REQUIRE(output.size() == 1);
+
+    auto instr = output[0];
+    REQUIRE(std::holds_alternative<ass::Instruction>(instr));
+
+    auto val = std::get<ass::Instruction>(instr);
+
+    REQUIRE(val.encodedHex == 0x301E);
+}
+
+TEST_CASE("SE 5xy0", "[parser]")
+{
+    ass::filename = "<test>";
+
+    ass::Lexer l{"SE V0, V1"};
+    const auto tokens = l.produceTokens();
+
+    ass::Parser p{};
+    p.parseLabels(tokens);
+    auto output = p.parseInstructions(tokens);
+
+    REQUIRE(output.size() == 1);
+
+    auto instr = output[0];
+    REQUIRE(std::holds_alternative<ass::Instruction>(instr));
+
+    auto val = std::get<ass::Instruction>(instr);
+
+    REQUIRE(val.encodedHex == 0x5010);
+}
+
+TEST_CASE("SHL 8xyE", "[parser]")
+{
+    ass::filename = "<test>";
+
+    ass::Lexer l{"SHL V0, V5"};
+    const auto tokens = l.produceTokens();
+
+    ass::Parser p{};
+    p.parseLabels(tokens);
+    auto output = p.parseInstructions(tokens);
+
+    REQUIRE(output.size() == 1);
+
+    auto instr = output[0];
+    REQUIRE(std::holds_alternative<ass::Instruction>(instr));
+
+    auto val = std::get<ass::Instruction>(instr);
+
+    REQUIRE(val.encodedHex == 0x805E);
+}
+
+TEST_CASE("SHR 8xy6", "[parser]")
+{
+    ass::filename = "<test>";
+
+    ass::Lexer l{"SHR V0, V5"};
+    const auto tokens = l.produceTokens();
+
+    ass::Parser p{};
+    p.parseLabels(tokens);
+    auto output = p.parseInstructions(tokens);
+
+    REQUIRE(output.size() == 1);
+
+    auto instr = output[0];
+    REQUIRE(std::holds_alternative<ass::Instruction>(instr));
+
+    auto val = std::get<ass::Instruction>(instr);
+
+    REQUIRE(val.encodedHex == 0x8056);
+}
+
+TEST_CASE("SKNP ExA1", "[parser]")
+{
+    ass::filename = "<test>";
+
+    ass::Lexer l{"SKNP VB"};
+    const auto tokens = l.produceTokens();
+
+    ass::Parser p{};
+    p.parseLabels(tokens);
+    auto output = p.parseInstructions(tokens);
+
+    REQUIRE(output.size() == 1);
+
+    auto instr = output[0];
+    REQUIRE(std::holds_alternative<ass::Instruction>(instr));
+
+    auto val = std::get<ass::Instruction>(instr);
+
+    REQUIRE(val.encodedHex == 0xEBA1);
+}
+
+TEST_CASE("SKP Ex9E", "[parser]")
+{
+    ass::filename = "<test>";
+
+    ass::Lexer l{"SKP VE"};
+    const auto tokens = l.produceTokens();
+
+    ass::Parser p{};
+    p.parseLabels(tokens);
+    auto output = p.parseInstructions(tokens);
+
+    REQUIRE(output.size() == 1);
+
+    auto instr = output[0];
+    REQUIRE(std::holds_alternative<ass::Instruction>(instr));
+
+    auto val = std::get<ass::Instruction>(instr);
+
+    REQUIRE(val.encodedHex == 0xEE9E);
+}
+
+TEST_CASE("SNE 4xnn", "[parser]")
+{
+    ass::filename = "<test>";
+
+    ass::Lexer l{"SNE V6, 0xAB"};
+    const auto tokens = l.produceTokens();
+
+    ass::Parser p{};
+    p.parseLabels(tokens);
+    auto output = p.parseInstructions(tokens);
+
+    REQUIRE(output.size() == 1);
+
+    auto instr = output[0];
+    REQUIRE(std::holds_alternative<ass::Instruction>(instr));
+
+    auto val = std::get<ass::Instruction>(instr);
+
+    REQUIRE(val.encodedHex == 0x46AB);
+}
+
+TEST_CASE("SNE 9xy0", "[parser]")
+{
+    ass::filename = "<test>";
+
+    ass::Lexer l{"SNE V0, VF"};
+    const auto tokens = l.produceTokens();
+
+    ass::Parser p{};
+    p.parseLabels(tokens);
+    auto output = p.parseInstructions(tokens);
+
+    REQUIRE(output.size() == 1);
+
+    auto instr = output[0];
+    REQUIRE(std::holds_alternative<ass::Instruction>(instr));
+
+    auto val = std::get<ass::Instruction>(instr);
+
+    REQUIRE(val.encodedHex == 0x90F0);
+}
+
+TEST_CASE("SUB 8xy5", "[parser]")
+{
+    ass::filename = "<test>";
+
+    ass::Lexer l{"SUB VF, VA"};
+    const auto tokens = l.produceTokens();
+
+    ass::Parser p{};
+    p.parseLabels(tokens);
+    auto output = p.parseInstructions(tokens);
+
+    REQUIRE(output.size() == 1);
+
+    auto instr = output[0];
+    REQUIRE(std::holds_alternative<ass::Instruction>(instr));
+
+    auto val = std::get<ass::Instruction>(instr);
+
+    REQUIRE(val.encodedHex == 0x8FA5);
+}
+
+TEST_CASE("SUBN 8xy7", "[parser]")
+{
+    ass::filename = "<test>";
+
+    ass::Lexer l{"SUBN V9, VA"};
+    const auto tokens = l.produceTokens();
+
+    ass::Parser p{};
+    p.parseLabels(tokens);
+    auto output = p.parseInstructions(tokens);
+
+    REQUIRE(output.size() == 1);
+
+    auto instr = output[0];
+    REQUIRE(std::holds_alternative<ass::Instruction>(instr));
+
+    auto val = std::get<ass::Instruction>(instr);
+
+    REQUIRE(val.encodedHex == 0x89A7);
+}
+
+TEST_CASE("XOR 8xy3", "[parser]")
+{
+    ass::filename = "<test>";
+
+    ass::Lexer l{"XOR VC, VD"};
+    const auto tokens = l.produceTokens();
+
+    ass::Parser p{};
+    p.parseLabels(tokens);
+    auto output = p.parseInstructions(tokens);
+
+    REQUIRE(output.size() == 1);
+
+    auto instr = output[0];
+    REQUIRE(std::holds_alternative<ass::Instruction>(instr));
+
+    auto val = std::get<ass::Instruction>(instr);
+
+    REQUIRE(val.encodedHex == 0x8CD3);
+}
